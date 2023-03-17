c8595b204ccb95ce4a72e72eabbf66637675cbed
MayconFelipeBianchi <114965402+MayconFelipeBianchi@users.noreply.github.com>
2023-03-17 19:53:38 -0300

    Add files via upload

---
+++ futebol/src/main/java/com/mycompany/futebol/NewClass.java
@@ -0,0 +1,38 @@
+public class Jogador {
+
+    private String nome;
+    private int idade;
+    private String posicao;
+    private boolean isLesionado;
+    private String time;
+    private boolean cadastrado = false;
+
+    public Jogador(String aNome, int aIdade, String aPosicao, boolean aIsLesionado, String aTime, boolean aCadastrado) {
+        this.nome = aNome;
+        this.idade = aIdade;
+        this.posicao = aPosicao;
+        this.isLesionado = aIsLesionado;
+        this.time = aTime;
+        this.cadastrado = aCadastrado;
+    }
+
+    public void getNomeIdade() {
+        System.out.println("O Jogador: " + nome + " Tem: " + idade + " Anos");
+    }
+
+    public void getPosicaoLesao() {
+        if (isLesionado) {
+            System.out.println("O Jogador: " + nome + " Joga na Posição: " + posicao + " E está atualmente lesionado");
+        } else {
+            System.out.println("O Jogador: " + nome + " Joga na Posição: " + posicao + " E está atualmente APTO PARA JOGO");
+        }
+    }
+
+    public void getTime() {
+        System.out.println("O Jogador: " + nome + " Joga no time: " + time);
+    }
+
+    public boolean isCadastrado(){
+        return this.cadastrado;
+    }
+}
